92e43be05e61204f1c7f93ab4243c872cb8016b7
Said <46088969+webmastersaid@users.noreply.github.com>
2023-09-09 19:39:34 +0500

    add isset tag_ids

--- app/Services/PostService.php
+++ app/Services/PostService.php
@@ -6,10 +6,11 @@ use App\Models\Post;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
 
-class PostService {
+class PostService
+{
     public function store($data)
     {
-        try{
+        try {
             DB::beginTransaction();
             if (isset($data['preview_image'])) {
                 $data['preview_image'] = Storage::disk('public')->put('/images', $data['preview_image']);
@@ -17,32 +18,40 @@ class PostService {
             if (isset($data['detail_image'])) {
                 $data['detail_image'] = Storage::disk('public')->put('/images', $data['detail_image']);
             }
-            $tagIds = $data['tag_ids'];
-            unset($data['tag_ids']);
+            if (isset($data['tag_ids'])) {
+                $tagIds = $data['tag_ids'];
+                unset($data['tag_ids']);
+            }
             $post = Post::firstOrCreate($data);
-            $post->tags()->attach($tagIds);
+            if (isset($tagIds)) {
+                $post->tags()->attach($tagIds);
+            }
             DB::commit();
-        }catch(\Exception $e){
+        } catch (\Exception $e) {
             DB::rollBack();
             abort(500);
         }
     }
     public function update($data, $post)
     {
-        try{
+        try {
             DB::beginTransaction();
-            $tagIds = $data['tag_ids'];
-            unset($data['tag_ids']);
             if (isset($data['preview_image'])) {
                 $data['preview_image'] = Storage::disk('public')->put('/images', $data['preview_image']);
             }
             if (isset($data['detail_image'])) {
                 $data['detail_image'] = Storage::disk('public')->put('/images', $data['detail_image']);
             }
+            if (isset($data['tag_ids'])) {
+                $tagIds = $data['tag_ids'];
+                unset($data['tag_ids']);
+            }
             $post->update($data);
-            $post->tags()->sync($tagIds);
+            if (isset($tagIds)) {
+                $post->tags()->sync($tagIds);
+            }
             DB::commit();
-        }catch(\Exception $e){
+        } catch (\Exception $e) {
             DB::rollBack();
             abort(500);
         }
