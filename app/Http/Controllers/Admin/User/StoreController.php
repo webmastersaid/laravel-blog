a417b46d6363f2f8248220e26e3fc93209758661
Said <46088969+webmastersaid@users.noreply.github.com>
2023-09-12 20:34:16 +0500

    add job

--- app/Http/Controllers/Admin/User/StoreController.php
+++ app/Http/Controllers/Admin/User/StoreController.php
@@ -4,22 +4,14 @@ namespace App\Http\Controllers\Admin\User;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\User\StoreRequest;
-use App\Mail\User\PasswordMail;
-use App\Models\User;
-use Illuminate\Auth\Events\Registered;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Mail;
+use App\Jobs\StoreUserJob;
 
 class StoreController extends Controller
 {
     public function __invoke(StoreRequest $request)
     {
         $data = $request->validated();
-        $password = $data['password'];
-        $data['password'] = Hash::make($data['password']);
-        $user = User::firstOrCreate(['email' => $data['email']],$data);
-        Mail::to($data['email'])->send(new PasswordMail($password));
-        event(new Registered($user));
+        StoreUserJob::dispatch($data);
         return redirect()->route('admin.user.index');
     }
 }
